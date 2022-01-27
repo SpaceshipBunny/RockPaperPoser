3e7a8ab04bab74bdd672c4dca753535c0f92373a
Yair Argaman <spaceshipbunny@gmail.com>
2022-01-27 15:26:18 +0200

    first commit
    Minor changes from the MediaPipe Hands JavaScript Solution API  example here:
    https://google.github.io/mediapipe/solutions/hands.html#javascript-solution-api

---
+++ index.js
@@ -0,0 +1,57 @@
+/*
+ Naming landmark indexes
+*/
+// const wristIndex = 0;
+// const indexFingerMCPIndex = 5;
+
+
+const videoElement = document.getElementsByClassName('input_video')[0];
+const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const canvasCtx = canvasElement.getContext('2d');
+
+function onResults(results) {
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.drawImage(
+      results.image, 0, 0, canvasElement.width, canvasElement.height);
+  if (results.multiHandLandmarks) {
+    for (const landmarks of results.multiHandLandmarks) {
+      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
+                     {color: '#00FF00', lineWidth: 5});
+      drawLandmarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 2});
+    }
+  }
+  canvasCtx.restore();
+}
+
+const hands = new Hands({locateFile: (file) => {
+  return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+}});
+hands.setOptions({
+  maxNumHands: 2,
+  modelComplexity: 1,
+  minDetectionConfidence: 0.8,
+  minTrackingConfidence: 0.8
+});
+hands.onResults(onResults);
+
+const camera = new Camera(videoElement, {
+  onFrame: async () => {
+    await hands.send({image: videoElement});
+  },
+  width: 1280,
+  height: 720
+});
+camera.start()
+
+    // function handShape(handLandmarks) {
+    //   if (pinkyFolded() && ringFingerFolded()) {
+    //     if (thumbFolded()) {
+    //       if (middleFingerFolded() && indexFingerFolded()) {
+    //         {
+    //           return rock;
+    //         }
+    //       } return scissors;
+    //     } return paper;
+    //   }
+    // }
